2504c2d880c5f7b9b7094f961bd8b9d8f082d9cb
Hyeji1364 <161557112+Hyeji1364@users.noreply.github.com>
2024-04-28 20:13:49 +0900

    upload

--- python/soribadaMusic.py
+++ python/soribadaMusic.py
@@ -1,38 +1,48 @@
-import requests as req
-from bs4 import BeautifulSoup as bs
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
-head = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
+import json
+from collections import OrderedDict
 
-res = req.get("https://www.soribada.com/music/chart", headers=head)
-
-
-# print(res.text)
-# print(res.status_code)  #406
-
-soup = bs(res.text, "lxml")
-
-# 데이터 선택
-ranking = soup.select(".number2 > .num")
-title = soup.select(".link type1 > .song title > title")
-artist = soup.select(".link-type2-name artist > a")
+# Chrome 옵션 설정
+options = ChromeOptions()
+options.add_argument("window-size=1920x1080")
+options.add_argument("disable-gpu")
+options.add_argument("lang=ko_KR")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
 
-print(len(ranking))
-print(len(title))
-print(len(artist))
 
-# 데이터 저장
-rankingList = [r.text.strip() for r in ranking]
-titleList = [t.text.strip() for t in title]
-artistList = [a.text.strip() for a in artist]
+# 사이트 접속
+driver.get('https://www.soribada.com/music/chart')
 
-#데이터 프레임 생성
-# chart_df = pd.DataFrame ({
-#     'Rank': rankingList,
-#     'Title' : titleList,
-#     'Artist': artistList
-# })
 
-# # JSON 파일로 저장
-# chart_df.to_json("soribada100.json", force_ascii=False, orient="records")
+time.sleep(2) 
+
+search_box = driver.find_element(By.CLASS_NAME, 'music-list')
+
+rankings = search_box.find_elements(By.CLASS_NAME , 'num')
+titles = search_box.find_elements(By.CLASS_NAME , 'song-title')
+artist = search_box.find_elements(By.CLASS_NAME ,  'link-type2-name')
+
+rankList = [rank.text for rank in rankings]
+titleList = [title.text for title in titles]
+artistList = [art.find_element(By.TAG_NAME , 'span').text for art in artist]
+
+
+# 데이터 프레임 생성
+chart_df = pd.DataFrame({
+    "Ranking" : rankList,
+    "Title" : titleList , 
+    "Artist" : artistList
+})
+
+
+chart_df.to_json("soriBadaMusicChart.json", force_ascii=False , orient="records")
